2243990086b4e9c010b158ce759ec2b6a1bfc50d
Nado <nado@troglodyte.be>
2017-05-17 19:07:37 +0200

    Modify makefile for packaging

--- linux/config.mk
+++ linux/config.mk
@@ -1,11 +1,12 @@
 # flags
-PREFIX  = /usr/local
+PREFIX  = /usr/local/
+LIBDIR = lib
 LIB	= librtinfo
 MAJOR	= 4
 VERSION = $(MAJOR).11
 
-CFLAGS  = -fpic -W -Wall -O2 -pipe -ansi -pedantic -std=gnu99 -DVERSION=$(VERSION)
-LDFLAGS = -shared -Wl,-soname,$(LIB).so.$(MAJOR)
+CFLAGS  += -fpic -W -Wall -O2 -pipe -ansi -pedantic -std=gnu99 -DVERSION=$(VERSION)
+LDFLAGS += -shared -Wl,-soname,$(LIB).so.$(MAJOR)
 LIBFILE = $(LIB).so.$(VERSION)
 
 CC = gcc
